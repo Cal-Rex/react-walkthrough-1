6c051991bb65aa18cdb2c0b61dc0278e50919763
Cal-Rex <andy.gibb@healthcareskills.com>
2023-07-26 19:01:25 +0000

    add setState function to StatefulGreeting component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
      <FunctionalGreeting />
      <FunctionalGreetingWithProps greeting="this was created using a prop" name="Andy" age="old"/>
+     <StatefulGreeting greeting="this is a stateful class component prop" name="Andy"/>
     </div>
   );
 }
